7f6713dac1359921b8df31fd7f22552196d61a7e
Johandrys <bermejovasquezjohandrys@gmail.com>
2026-06-26 21:08:03 -0500

    Crear aplicacion Flask basica

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask as fk
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return fk.render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
