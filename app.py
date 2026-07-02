1c6a339ea4baa15c2971a532f3b56dec36b22d07
Johandrys <bermejovasquezjohandrys@gmail.com>
2026-07-02 15:55:19 -0500

    Migrar gestión de tareas a SQLite

--- app.py
+++ app.py
@@ -1,9 +1,55 @@
 import flask as fk
 from flask import Flask,render_template, redirect, url_for
 
+import sqlite3
+
 app = Flask(__name__)
 
-tareas = []
+DATABASE = "instance/tareas.db"
+def crear_base_datos():
+    conexion = sqlite3.connect(DATABASE)
+    cursor = conexion.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tareas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            completada INTEGER NOT NULL
+        )
+    ''')
+    conexion.commit()
+    conexion.close()
+
+def obtener_tareas():
+    conexion = sqlite3.connect(DATABASE)
+
+    conexion.row_factory = sqlite3.Row
+
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM tareas")
+    tareas = cursor.fetchall()
+    conexion.close()
+    return tareas
+
+def agregar_tarea(titulo):
+    conexion = sqlite3.connect(DATABASE)
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO tareas (titulo, completada) VALUES (?, ?)", (titulo, 0))
+    conexion.commit()
+    conexion.close()
+
+def eliminar_tarea(id_tarea):
+    conexion = sqlite3.connect(DATABASE)
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM tareas WHERE id = ?", (id_tarea,))
+    conexion.commit()
+    conexion.close()
+
+def completar_tarea(id_tarea):
+    conexion = sqlite3.connect(DATABASE)
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE tareas SET completada = NOT completada WHERE id = ?", (id_tarea,))
+    conexion.commit()
+    conexion.close()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,14 +57,11 @@ def home():
         tarea = fk.request.form["tarea"].strip()
 
         if tarea:
-            tareas.append(
-                {
-                    "titulo": tarea,
-                    "completada": False
-                }
-            )
+            agregar_tarea(tarea)
         
         return redirect(url_for("home"))
+    
+    tareas = obtener_tareas()
 
     return render_template(
         "index.html",
@@ -27,15 +70,18 @@ def home():
 
 @app.route("/completar/<int:id_tarea>", methods=["POST"])
 def completar(id_tarea):
-    if 0 <= id_tarea < len(tareas):
-        tareas[id_tarea]["completada"] = not tareas[id_tarea]["completada"]
+    completar_tarea(id_tarea)
+
     return redirect(url_for("home"))
 
 @app.route("/eliminar/<int:id_tarea>", methods=["POST"])
 def eliminar(id_tarea):
-    if 0 <= id_tarea < len(tareas):
-        tareas.pop(id_tarea)
+    eliminar_tarea(id_tarea)
+
     return redirect(url_for("home"))
+
+
+crear_base_datos()
 
 
 if __name__ == "__main__":
